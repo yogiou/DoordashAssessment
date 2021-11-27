0392bbb75f3338effbe74f6e10c1a9496786b6f8
Wen <jie.wen@live.hk>
2021-11-28 00:24:30 +0800

    Initial commit

---
+++ src/test/java/jie/wen/doordash/assessment/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jie.wen.doordash.assessment.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
